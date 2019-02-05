5ae962ef46c4e2f6995f578d2f0cae04b398ceb9
Previn Wong <previnwong@Previns-MacBook-Air.local>
2019-02-04 16:10:11 -0800

    test

--- main.py
+++ main.py
@@ -110,13 +110,25 @@ def get():
 	# datMoney = AudioSegment.from_file("./mp3s/ivr.mp3", format="mp3")
 	# play(datMoney)
 
-	# return send_from_directory(app.static_folder, ivr_id)
+	return send_from_directory(app.static_folder, ivr_id)
 
-	return jsonify(value=ivr_url)
+	# return jsonify(value=ivr_url)
 
 # @app.route('/app')
 # def app():
 # 	return render_template('index.html')
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 if __name__ == '__main__':
     app.run(debug=True)
